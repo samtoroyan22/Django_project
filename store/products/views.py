5d0443ba1579d1131fcc62609e77a560e293815f
Тороян Самвел <toroyan_s@itstep.loc>
2024-09-16 21:44:13 +0300

    add index.html, products.html

--- store/products/views.py
+++ store/products/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "products/index.html")
+
+
+def products(request):
+    return render(request, "products/products.html")
+
+
+def test_context(request):
+
+    context = {
+        "title": "store",
+        "header": "hello world",
+        "products": [
+            {"name": "product-1", "price": 2000},
+            {"name": "product-2", "price": 5000},
+            {"name": "product-3", "price": 10000},
+        ],
+        "sale_products": ["product-4", "product-5"]
+    }
+
+    return render(request, "products/test_context.html", context)

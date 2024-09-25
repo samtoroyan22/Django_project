55dba3369d11d34c6440db8fb8b5c6ba29ad53d5
Тороян Самвел <toroyan_s@itstep.loc>
2024-09-25 20:41:42 +0300

    add app users

--- store/store/settings.py
+++ store/store/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,4 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
 

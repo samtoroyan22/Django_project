0e7574997bb6104578615d47862da4368156b5ed
Тороян Самвел <toroyan_s@itstep.loc>
2024-10-02 21:12:38 +0300

    add logout, UserProfileForm, errors and messages

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from users.models import User
 from django import forms
 
@@ -53,4 +53,30 @@ class UserRegistrationForm(UserCreationForm):
         fields = ("first_name", "last_name", "username", "email", "password1", "password2")
 
 
+class UserProfileForm(UserChangeForm):
 
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control py-4",
+        "readonly": True,
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        "class": "form-control py-4",
+        "readonly": True,
+    }))
+
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control py-4",
+    }))
+
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "form-control py-4",
+    }))
+
+    image = forms.ImageField(widget=forms.FileInput(attrs={
+        "class": "custom-file-label",
+    }))
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name", "image")
--- store/users/views.py
+++ store/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
-from django.contrib import auth
-from users.forms import UserLoginForm, UserRegistrationForm
+from django.contrib import auth, messages
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -28,6 +28,7 @@ def register(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, message="Вы успешно зарегались")
             return HttpResponseRedirect(reverse('users:login'))
         else:
             print(form.errors)
@@ -35,3 +36,22 @@ def register(request):
         form = UserRegistrationForm()
     context = {"form": form}
     return render(request, "users/register.html", context)
+
+
+def profile(request):
+    if request.method == "POST":
+        form = UserProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {"form": form}
+    return render(request, "users/profile.html", context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
